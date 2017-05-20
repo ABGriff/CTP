f5751f21177e824ba9bd22a5e3b93f6c651c65ee
Alexander Burey <bureyalexander@gmail.com>
2017-05-21 01:50:13 +1000

    Fix Hologram FBX Import Error
    Some FBX files were not loading correctly for an unknown reason.

--- Application/CTP/App.cs
+++ Application/CTP/App.cs
@@ -8,86 +8,85 @@ using UnityPlayer;
 
 namespace CTP
 {
-	class App : IFrameworkView, IFrameworkViewSource
-	{
-		private WinRTBridge.WinRTBridge m_Bridge;
-		private AppCallbacks m_AppCallbacks;
+    class App : IFrameworkView, IFrameworkViewSource
+    {
+        private WinRTBridge.WinRTBridge m_Bridge;
+        private AppCallbacks m_AppCallbacks;
 
-		public App()
-		{
-			SetupOrientation();
-			m_AppCallbacks = new AppCallbacks();
+        public App()
+        {
+            SetupOrientation();
+            m_AppCallbacks = new AppCallbacks();
 
-			// Allow clients of this class to append their own callbacks.
-			AddAppCallbacks(m_AppCallbacks);
-		}
+            // Allow clients of this class to append their own callbacks.
+            AddAppCallbacks(m_AppCallbacks);
+        }
 
-		public virtual void Initialize(CoreApplicationView applicationView)
-		{
-			applicationView.Activated += ApplicationView_Activated;
-			CoreApplication.Suspending += CoreApplication_Suspending;
+        public virtual void Initialize(CoreApplicationView applicationView)
+        {
+            applicationView.Activated += ApplicationView_Activated;
+            CoreApplication.Suspending += CoreApplication_Suspending;
 
-			// Setup scripting bridge
-			m_Bridge = new WinRTBridge.WinRTBridge();
-			m_AppCallbacks.SetBridge(m_Bridge);
+            // Setup scripting bridge
+            m_Bridge = new WinRTBridge.WinRTBridge();
+            m_AppCallbacks.SetBridge(m_Bridge);
 
-			m_AppCallbacks.SetCoreApplicationViewEvents(applicationView);
-		}
+            m_AppCallbacks.SetCoreApplicationViewEvents(applicationView);
+        }
 
-		/// <summary>
-		/// This is where apps can hook up any additional setup they need to do before Unity intializes.
-		/// </summary>
-		/// <param name="appCallbacks"></param>
-		virtual protected void AddAppCallbacks(AppCallbacks appCallbacks)
-		{
-		}
+        /// <summary>
+        /// This is where apps can hook up any additional setup they need to do before Unity intializes.
+        /// </summary>
+        /// <param name="appCallbacks"></param>
+        virtual protected void AddAppCallbacks(AppCallbacks appCallbacks)
+        {
+        }
 
-		private void CoreApplication_Suspending(object sender, SuspendingEventArgs e)
-		{
+        private void CoreApplication_Suspending(object sender, SuspendingEventArgs e)
+        {
+        }
 
-		}
+        private void ApplicationView_Activated(CoreApplicationView sender, IActivatedEventArgs args)
+        {
+            CoreWindow.GetForCurrentThread().Activate();
+        }
 
-		private void ApplicationView_Activated(CoreApplicationView sender, IActivatedEventArgs args)
-		{
-			CoreWindow.GetForCurrentThread().Activate();
-		}
+        public void SetWindow(CoreWindow coreWindow)
+        {
+            ApplicationView.GetForCurrentView().SuppressSystemOverlays = true;
 
-		public void SetWindow(CoreWindow coreWindow)
-		{
-			ApplicationView.GetForCurrentView().SuppressSystemOverlays = true;
+            m_AppCallbacks.SetCoreWindowEvents(coreWindow);
+            m_AppCallbacks.InitializeD3DWindow();
+        }
 
-			m_AppCallbacks.SetCoreWindowEvents(coreWindow);
-			m_AppCallbacks.InitializeD3DWindow();
-		}
+        public void Load(string entryPoint)
+        {
+        }
 
-		public void Load(string entryPoint)
-		{
-		}
+        public void Run()
+        {
+            m_AppCallbacks.Run();
+        }
 
-		public void Run()
-		{
-			m_AppCallbacks.Run();
-		}
+        public void Uninitialize()
+        {
+        }
 
-		public void Uninitialize()
-		{
-		}
+        [MTAThread]
+        static void Main(string[] args)
+        {
+            var app = new App();
+            CoreApplication.Run(app);
+        }
 
-		[MTAThread]
-		static void Main(string[] args)
-		{
-			var app = new App();
-			CoreApplication.Run(app);
-		}
+        public IFrameworkView CreateView()
+        {
+            return this;
+        }
 
-		public IFrameworkView CreateView()
-		{
-			return this;
-		}
-
-		private void SetupOrientation()
-		{
-			Unity.UnityGenerated.SetupDisplay();
-		}
-	}
+        private void SetupOrientation()
+        {
+            Unity.UnityGenerated.SetupDisplay();
+        }
+    }
 }
