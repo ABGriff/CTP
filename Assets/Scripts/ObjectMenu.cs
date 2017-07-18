907cb63dcd6591a2f05328d86d7b7224f607f659
Alexander Burey <bureyalexander@gmail.com>
2017-07-18 21:17:52 +1000

    Basic Object Menu Complete
    Added a basic menu for correctly spawning objects.
    TODO:
    Rigidbody (Assign To Prefab)
    Design (Later Date)
    Grabbable

---
+++ Assets/Scripts/ObjectMenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class ObjectMenu : MonoBehaviour
+{
+
+
+    public void SpawnSphere()
+    {
+        GameObject go = Instantiate(Resources.Load("Sphere")) as GameObject;
+    }
+
+    public void SpawnCube()
+    {
+        GameObject go = Instantiate(Resources.Load("Cube")) as GameObject;
+    }
+}
+
