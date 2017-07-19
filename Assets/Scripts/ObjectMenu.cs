b75a8062830663f7b402110c3f776286fc3d481f
Alexander Burey <bureyalexander@gmail.com>
2017-07-19 21:15:41 +1000

    Menu Update
    Made it a little prettier with a background, added in a car spawner
    (That spawns the car correctly now....)
    Ungrabbable yet

--- Assets/Scripts/ObjectMenu.cs
+++ Assets/Scripts/ObjectMenu.cs
@@ -9,12 +9,17 @@ public class ObjectMenu : MonoBehaviour
 
     public void SpawnSphere()
     {
-        GameObject go = Instantiate(Resources.Load("Sphere")) as GameObject;
+        GameObject go = Instantiate(Resources.Load("Sphere"), new Vector3(0.1F,0.01F,3F), transform.rotation) as GameObject;
     }
 
     public void SpawnCube()
     {
-        GameObject go = Instantiate(Resources.Load("Cube")) as GameObject;
+        GameObject go = Instantiate(Resources.Load("Cube"), new Vector3(0.2F, 0.02F, 3F), transform.rotation) as GameObject;
+    }
+
+    public void SpawnCar()
+    {
+        GameObject go = Instantiate(Resources.Load("Car"), new Vector3(0.2F, 0.02F, 3F), Quaternion.Euler(new Vector3(-90, 0, 0))) as GameObject;
     }
 }
 
